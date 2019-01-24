bb91b949c85377c057b528f03bd66596eb80fe43
divergent_cix <tolu58@gmail.com>
2019-01-24 16:12:20 +0100

    Update settings.py

--- cbt/cbt/settings.py
+++ cbt/cbt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hyzd@maja)qcrf77*mad@kzywugdp&()u3499bwtw!6(+(z@=9'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
